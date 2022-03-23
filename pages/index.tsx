6db724db17523bb654a4b841d93c4dc1965a1031
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-03-22 21:43:54 -0600

    tipo de respuesta de axios con interfaces

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import { NextPage, GetStaticProps } from "next";
 import { pokeApi } from "../api";
 import { Layout } from "../components/layouts";
+import { PokemonListResponse } from "../interfaces";
 
 const HomePage: NextPage = (props) => {
-
     return (
         <Layout title="Listado de Pókemons">
             <ul>
@@ -14,7 +14,9 @@ const HomePage: NextPage = (props) => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-    const { data } = await pokeApi.get("/pokemon?limit=151");
+    const { data } = await pokeApi.get<PokemonListResponse>(
+        "/pokemon?limit=151"
+    );
 
     return {
         props: { pokemons: data.results },
