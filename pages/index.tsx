b6cc0599cd48b88feab51f6c80c94e92db3ebda8
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-03-22 21:16:44 -0600

    getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
-import { NextPage } from "next";
+import { NextPage, GetStaticProps } from "next";
 import { Button } from "@nextui-org/react";
 import { Layout } from "../components/layouts";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+    console.log(props);
+
     return (
         <Layout title="Listado de Pókemons">
-            <Button color="gradient">Hola mundo</Button>
+            <ul>
+                <li>Pokemon</li>
+            </ul>
         </Layout>
     );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    console.log("hola mundo");
+
+    return {
+        props: {},
+    };
 };
 
 export default HomePage;
